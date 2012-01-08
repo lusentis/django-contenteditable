2768eec090a419f939a9092870f61a7e17d3e847
Simone Lusenti <lusenti.s@gmail.com>
2012-01-08 21:33:09 +0100

    Replaced content_update with content_update_from_dict
    Now object attributes are updated directly from a dictionary and given
    keys

--- contenteditable/utils.py
+++ contenteditable/utils.py
@@ -3,31 +3,35 @@
 from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import get_object_or_404
 
-def content_update(Model, **kwargs):
-	"""
-	This method should be used for testing purposes only.
-	I don't think using __setattr__ it's a good idea...
-	"""
-	if kwargs['pk']=='-1':
-		obj = Model()
-	else:
-		obj = get_object_or_404(Model, pk=kwargs['pk'])
-	
-	for arg in kwargs:
-		if arg == 'pk': continue	# don't set pkeys
-		if kwargs[arg] is None: continue # don't set if not passed
 
-		try:
-			obj.__setattr__(arg, kwargs[arg])
-		except AttributeError, e:
-			raise ValueError('Passed invalid arg `{0}`'.format(arg))	
+def content_update_from_dict(Model, source_dict, keys):
+    """
+    Updates Model with values from source_dict specified by keys
+    """
+    
+    if source_dict.get('id') is None:
+        raise ValueError('Source dict didn\'t get a value for id.')
+    elif source_dict.get('id')=='-1':
+	    obj = Model()
+    else:
+	    obj = get_object_or_404(Model, pk=source_dict.get('id'))
 
-	obj.save()
-	return obj.pk
+    for k in keys:
+        if k == 'pk': continue	# don't set pkeys
+        if source_dict[k] is None: continue # don't set if not passed
+
+        try:
+            obj.__setattr__(k, source_dict[k])
+        except AttributeError, e:
+            raise ValueError('Passed invalid arg `{0}`'.format(k))	
+
+    obj.save()
+    return obj.pk
+
 
 def content_delete(Model, pk):
-	obj = get_object_or_404(Model, pk=pk)
-	obj.delete()
-	return True
+    obj = get_object_or_404(Model, pk=pk)
+    obj.delete()
+    return True
 
 
