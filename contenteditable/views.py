872a7abc9a5d5775a5c3923b4bd83b477b2f5422
Simone Lusenti <lusenti.s@gmail.com>
2012-01-08 21:33:55 +0100

    Replaced existing code with generic configuration-based code

--- contenteditable/views.py
+++ contenteditable/views.py
@@ -4,42 +4,35 @@ from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from contenteditable.utils import content_update, content_delete
-
+from contenteditable.utils import content_update_from_dict, content_delete
 from newspaper.models import Article
 
+from contenteditablesettings import CONTENTEDITABLE_MODELS
+
+
 @csrf_exempt
 @require_POST
-#@login_required	### UNCOMMENT THIS!
+#@login_required        ### UNCOMMENT THIS!
 def update_view(request):
-	"""
-	request.POST contains:
-		- model
-		- id
-		- data*
-	"""
-	if request.POST.get('model')=='article':
-		if not content_update(Article, 
-				pk=request.POST.get('id').strip(),
-				title=request.POST.get('title'),
-				text=request.POST.get('text')):
-			return HttpResponseServerError('Content cannot be updated')
-	else:
-		raise ValueError('Unknown model: {0}'.format(request.POST.get('model')))
-
-	return HttpResponse('ok');
+    model = request.POST.get('model')
+    if CONTENTEDITABLE_MODELS.get(model) is not None:
+        e_conf = CONTENTEDITABLE_MODELS[model]
+        if content_update_from_dict(e_conf[0], request.POST, e_conf[1]):
+            return HttpResponse('ok')
+        else:
+            return HttpResponseServerError('Content cannot be updated')
+    else:
+        raise ValueError('Unknown model: {0}'.format(request.POST.get('model')))
 
 
 @csrf_exempt
 @require_POST
-#@login_required	### UNCOMMENT THIS!
+#@login_required        ### UNCOMMENT THIS!
 def delete_view(request):
-	if request.POST.get('model')=='article':
-		if not content_delete(Article, pk=request.POST.get('id').strip()):
-			return HttpResponseServerError('Delete failed')
-	else:
-		raise ValueError('Unknown model: {0}'.format(request.POST.get('model')))
-
-	return HttpResponse('ok')
-	
+    model = request.POST.get('model')
+    if CONTENTEDITABLE_MODELS.get(model) is not None: 
+        content_delete(CONTENTEDITABLE_MODELS[model][0], pk=request.POST.get('id'))
+        return HttpResponse('ok')
+    else:
+        raise ValueError('Unknown model: {0}'.format(request.POST.get('model')))
 
