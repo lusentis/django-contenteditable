b8ed6174a1b08fcbcd3d6acb6540cfbe5130d00d
crccheck <c@crccheck.com>
2012-06-23 16:26:22 +0000

    refactor sample models
    pep8 whitespace
    unicode method returned string, not unicode

--- sample/newspaper/models.py
+++ sample/newspaper/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Article(models.Model):
-	title = models.CharField(max_length=200)
-	text = models.TextField(default='This article has no text', blank=True, null=True)
+    title = models.CharField(max_length=200)
+    text = models.TextField(default='This article has no text',
+                            blank=True, null=True)
 
-	def __unicode__(self):
-		return "{0}".format(self.title)
-
+    def __unicode__(self):
+        return self.title
