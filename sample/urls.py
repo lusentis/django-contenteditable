d3d417531ce1c616d26164b62dbba1770947b471
crccheck <c@crccheck.com>
2012-06-24 01:06:40 +0000

    add auth to sample project urls

--- sample/urls.py
+++ sample/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^contenteditable/', include('contenteditable.urls')),
     url(r'', include('newspaper.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 )
