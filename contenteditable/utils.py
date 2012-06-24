b746df3226642eb27c667c912be5982e17cdda50
crccheck <c@crccheck.com>
2012-06-24 04:06:33 +0000

    pep8 utils.py

--- contenteditable/utils.py
+++ contenteditable/utils.py
@@ -10,18 +10,20 @@ def content_update_from_dict(Model, source_dict, keys):
 
     if source_dict.get('id') is None:
         raise ValueError('Source dict didn\'t get a value for id.')
-    elif source_dict.get('id')=='-1':
+    elif source_dict.get('id') == '-1':
         obj = Model()
     else:
         obj = get_object_or_404(Model, pk=source_dict.get('id'))
 
     for k in keys:
-        if k == 'pk': continue  # don't set pkeys
-        if source_dict[k] is None: continue # don't set if not passed
+        if k == 'pk':
+            continue  # don't set pkeys
+        if source_dict[k] is None:
+            continue  # don't set if not passed
 
         try:
             obj.__setattr__(k, source_dict[k])
-        except AttributeError, e:
+        except AttributeError:
             raise ValueError('Passed invalid arg `{0}`'.format(k))
 
     obj.save()
