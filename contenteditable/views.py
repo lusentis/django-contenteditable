5b86daf8120850b891626b9ff231f4ecc120c3a3
crccheck <c@crccheck.com>
2012-06-24 01:34:52 +0000

    convert update view to a CBV

--- contenteditable/views.py
+++ contenteditable/views.py
@@ -1,24 +1,26 @@
 from django.http import HttpResponse, HttpResponseServerError
 from django.contrib.auth.views import login_required
 from django.views.decorators.http import require_POST
+from django.views.generic import View
 
 from contenteditable.utils import content_update_from_dict, content_delete
 
 from contenteditablesettings import CONTENTEDITABLE_MODELS
 
 
-@require_POST
-#@login_required        ### UNCOMMENT THIS!
-def update_view(request):
-    model = request.POST.get('model')
-    if CONTENTEDITABLE_MODELS.get(model) is not None:
-        e_conf = CONTENTEDITABLE_MODELS[model]
-        if content_update_from_dict(e_conf[0], request.POST, e_conf[1]):
-            return HttpResponse('ok')
+class UpdateView(View):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        model = request.POST.get('model')
+        if CONTENTEDITABLE_MODELS.get(model) is not None:
+            e_conf = CONTENTEDITABLE_MODELS[model]
+            if content_update_from_dict(e_conf[0], request.POST, e_conf[1]):
+                return HttpResponse('ok')
+            else:
+                return HttpResponseServerError('Content cannot be updated')
         else:
-            return HttpResponseServerError('Content cannot be updated')
-    else:
-        raise ValueError('Unknown model: {0}'.format(request.POST.get('model')))
+            raise ValueError('Unknown model: {0}'.format(request.POST.get('model')))
 
 
 @require_POST
