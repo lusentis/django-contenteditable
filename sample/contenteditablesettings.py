bfd43d325f7f9b45de10d014f81b79468e2de764
crccheck <c@crccheck.com>
2012-07-19 05:45:54 +0000

    change settings format to tuple of tuples and names, not actual models

--- sample/contenteditablesettings.py
+++ sample/contenteditablesettings.py
@@ -1,9 +1,6 @@
 """ Defines Models that are allowed to be edited using contenteditable """
 
-from newspaper.models import Article
-from chunks.models import Chunk
-
-CONTENTEDITABLE_MODELS = {
-    'article': (Article, ('title', 'text')),
-    'chunk': (Chunk, ('content',)),
-}
+CONTENTEDITABLE_MODELS = (
+    ('newspaper.article', ('title', 'text')),
+    ('chunks.chunk', ('content',)),
+)
