d261e77c74ce327c1104201b9e655ad888356546
crccheck <c@crccheck.com>
2012-07-29 18:24:46 +0000

    move and update settings docs

--- contenteditable/settings.py
+++ contenteditable/settings.py
@@ -1,7 +1,21 @@
 from django.conf import settings
 
+# to enable or disable contenteditable functionality. affects the
+# templatetags and the views. You may not want this enabled in production
+# because of caching and exposing primary keys.
 CONTENTEDITABLE_ENABLED = getattr(settings, 'CONTENTEDITABLE_ENABLED',
     settings.DEBUG)
+
+# define models and their fields that are allowed to be edited
+# you must opt-in and explicitly allow each model and field like:
+# CONTENTEDITABLE_MODELS = (
+#     ('newspaper.article', ('title', 'text')),
+#     ('chunks.chunk', ('content',)),
+# )
+CONTENTEDITABLE_MODELS = None
+
+# CONTENTEDITABLE_MODELS gets transformered below into two internally used
+# settings: editable_models and e_models
 
 try:
     if not CONTENTEDITABLE_ENABLED:
@@ -16,4 +30,3 @@ try:
 except AttributeError:
     editable_models = None
     e_models = None
-
