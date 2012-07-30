e926e59dced75100fe761f33f3fcecdf26ab3b64
crccheck <c@crccheck.com>
2012-07-30 02:02:26 +0000

    add test to make sure we can do updates

--- contenteditable/tests/api.py
+++ contenteditable/tests/api.py
@@ -2,33 +2,54 @@ from django.test import TestCase
 from django.utils import unittest
 from django.core.urlresolvers import reverse
 
-# from sample.newspaper.models import Article
+from sample.newspaper.models import Article
 
 
-class HTTPMethodsTestCase(TestCase):
+class BaseTestCase(TestCase):
     fixtures = ['test_auth.json']
 
+    def setUp(self):
+        self.obj = Article.objects.get(pk=1)
+        self.url = reverse('dce_endpoint')
+        self.base_data = {'model': 'article', 'pk': '1'}
+
+    def generate(self, updates):
+        new_data = self.base_data.copy()
+        new_data.update(updates)
+        return new_data
+
+
+class LoggedInTestCase(BaseTestCase):
+    def setUp(self):
+        super(LoggedInTestCase, self).setUp()
+        self.client.login(username='test', password='test')
+
+
+class HTTPMethodsTestCase(LoggedInTestCase):
     def test_get_is_not_allowed(self):
-        url = reverse('dce_endpoint')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 405)
 
     def test_post_is_allowed(self):
-        url = reverse('dce_endpoint')
-        self.client.login(username='test', password='test')
-        response = self.client.post(url, {'model': 'article', 'pk': '1'})
+        response = self.client.post(self.url, self.base_data)
         self.assertEqual(response.status_code, 200)
 
     @unittest.expectedFailure
     def test_put_is_allowed(self):
-        url = reverse('dce_endpoint')
-        self.client.login(username='test', password='test')
-        response = self.client.put(url, {'model': 'article', 'pk': '1'})
+        response = self.client.put(self.url, self.base_data)
         self.assertEqual(response.status_code, 200)
 
     @unittest.expectedFailure
     def test_delete_is_allowed(self):
-        url = reverse('dce_endpoint')
-        self.client.login(username='test', password='test')
-        response = self.client.delete(url, {'model': 'article', 'pk': '1'})
+        response = self.client.delete(self.url, self.base_data)
         self.assertEqual(response.status_code, 200)
+
+
+class Updates(LoggedInTestCase):
+    def test_can_update_field(self):
+        new_title = "Poopity Poop Pooh"
+        response = self.client.post(self.url,
+                   self.generate({'title': new_title}))
+        self.assertEqual(response.status_code, 200)
+        obj = Article.objects.get(pk=1)
+        self.assertEqual(obj.title, new_title)
